1ae42ca48dcfc4f9a72634644a9f247e7cb53300
hendisantika <hendisantika@yahoo.co.id>
2026-07-18 08:38:37 +0700

    feat: add update_item by ID in routers.py🫸🌀✏️📗🐧🐳⬆

--- routers.py
+++ routers.py
@@ -19,3 +19,18 @@ def get_item(item_id: int):
             return item
 
     return {"error": "Item not found"}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, updated_item: Item):
+    for index, item in enumerate(items):
+
+        if item.id == item_id:
+            items[index] = updated_item
+
+            return {
+                "message": "Item updated successfully",
+                "data": updated_item
+            }
+
+    return {"error": "Item not found"}
